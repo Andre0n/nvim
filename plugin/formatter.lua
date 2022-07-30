6b72c6fcca912e4dde3fa593b0626eb501d67001
Andre0n <andreon098@gmail.com>
2022-07-30 14:09:31 -0300

    Add new Formatter options.

--- plugin/formatter.lua
+++ plugin/formatter.lua
@@ -14,23 +14,7 @@ formatter.setup({
         -- and will be executed in order
         lua = {
             require("formatter.filetypes.lua").stylua,
-             -- You can also define your own configuration
-            -- function()
-            --     -- Full specification of configurations is down below and in Vim help
-            --     -- files
-            --     return {
-            --         exe = "stylua",
-            --         args = {
-            --             "--search-parent-directories",
-            --             "--stdin-filepath",
-            --              util.escape_path(util.get_current_buffer_file_path()),
-            --             "--",
-            --             "-",
-            --         },
-            --         stdin = true,
-            --     }
-            -- end
-        },
+       },
         rust  = {
             require("formatter.filetypes.rust").rustfmt,
         },
@@ -39,6 +23,12 @@ formatter.setup({
         },
         cpp  = {
             require("formatter.filetypes.cpp").clangformat,
+        },
+        html = {
+             require("formatter.filetypes.html").prettier,
+        },
+        css = {
+             require("formatter.filetypes.css").prettier,
         }
     }
 })
