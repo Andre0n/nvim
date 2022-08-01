9d77973a600bca5d5b10f4cc172b3897872e5475
Andre0n <andreon098@gmail.com>
2022-08-01 09:28:35 -0300

    Update `treesitter` options.

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -1,16 +1,13 @@
 local exists, treesitter = pcall(require, "nvim-treesitter.configs")
 
 if not exists then
-    return
+	return
 end
 
 treesitter.setup({
-    ensure_installed = { "c", "lua" },
-    highlight = {
-        enable = true,
-        use_languagetree = true
-    },
-    indent = {
-        enable = true,
-    }
+	ensure_installed = { "c", "cpp", "lua", "python" },
+	highlight = {
+		enable = true,
+		use_languagetree = true,
+	},
 })
