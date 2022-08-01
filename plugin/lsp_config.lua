eb185fca024928bb5fe9f6198afd88bab7b8ae38
Andre0n <andreon098@gmail.com>
2022-08-01 10:55:40 -0300

    Update plugin configuration `lsp_config.lua`

--- plugin/lsp_config.lua
+++ plugin/lsp_config.lua
@@ -1,70 +1,96 @@
-local exists, lspconfig = pcall(require, "lsp-config")
+local exists, lspconfig = pcall(require, "lspconfig")
 
 if not exists then
-	return
+    return
 end
 
-local function lspSymbol(name, icon)
-   local hl = "DiagnosticSign" .. name
-   vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
-end
-
-lspSymbol("Error", "")
-lspSymbol("Info", "")
-lspSymbol("Hint", "")
-lspSymbol("Warn", "")
-
-vim.diagnostic.config {
-   virtual_text = {
-      prefix = "",
-   },
-   signs = true,
-   underline = true,
-   update_in_insert = false,
-}
+vim.diagnostic.config({
+    virtual_text = {
+        prefix = "",
+    },
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+})
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-   border = "single",
+    border = "single",
 })
 
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-   border = "single",
-   focusable = false,
-   relative = "cursor",
+    border = "single",
+    focusable = false,
+    relative = "cursor",
 })
 
 -- suppress error messages from lang servers
 vim.notify = function(msg, log_level)
-   if msg:match "exit code" then
-      return
-   end
-   if log_level == vim.log.levels.ERROR then
-      vim.api.nvim_err_writeln(msg)
-   else
-      vim.api.nvim_echo({ { msg } }, true, {})
-   end
+    if msg:match("exit code") then
+        return
+    end
+    if log_level == vim.log.levels.ERROR then
+        vim.api.nvim_err_writeln(msg)
+    else
+        vim.api.nvim_echo({ { msg } }, true, {})
+    end
 end
 
 -- Borders for LspInfo winodw
-local win = require "lspconfig.ui.windows"
+local win = require("lspconfig.ui.windows")
 local _default_opts = win.default_opts
 
 win.default_opts = function(options)
-   local opts = _default_opts(options)
-   opts.border = "single"
-   return opts
+    local opts = _default_opts(options)
+    opts.border = "single"
+    return opts
 end
 
-exists, cmp_lsp = pcall(require, "cmp_nvim_lsp")
-local capabilities = {}
-
-if exists then
-	capabilities = cmp_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local function get_capabilities()
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.completion.completionItem = {
+        documentationFormat = { "markdown", "plaintext" },
+        snippetSupport = true,
+        preselectSupport = true,
+        insertReplaceSupport = true,
+        labelDetailsSupport = true,
+        deprecatedSupport = true,
+        commitCharactersSupport = true,
+        tagSupport = { valueSet = { 1 } },
+        resolveSupport = {
+            properties = {
+                "documentation",
+                "detail",
+                "additionalTextEdits",
+            },
+        },
+    }
+    local cmp_lsp = {}
+    exists, cmp_lsp = pcall(require, "cmp_nvim_lsp")
+    if exists then
+        capabilities = cmp_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    end
+    return capabilities
 end
 
-local lsp_servers = { "sumneko_lua", "clangd", "html"}
+local diagnostics = {
+    signs = {
+        active = true,
+        values = {
+            { name = "DiagnosticSignError", text = "" },
+            { name = "DiagnosticSignWarn", text = "" },
+            { name = "DiagnosticSignHint", text = "" },
+            { name = "DiagnosticSignInfo", text = "" },
+        },
+    },
+}
+
+for _, sign in ipairs(diagnostics.signs.values) do
+    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
+end
+
+local lsp_servers = { "sumneko_lua", "clangd", "pyright" }
 for _, server in ipairs(lsp_servers) do
-	lspconfig[server].setup({
-		capabilities = capabilities,
-	})
+    lspconfig[server].setup({
+        capabilities = get_capabilities(),
+    })
 end
