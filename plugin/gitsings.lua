89f4404939c6c54373e691939906968fa41184ed
Andre0n <andreon098@gmail.com>
2022-08-01 09:41:25 -0300

    Format `gitsings` config file

--- plugin/gitsings.lua
+++ plugin/gitsings.lua
@@ -1,16 +1,15 @@
 local exists, gitsigns = pcall(require, "gitsigns")
 
 if not exists then
-    return
+	return
 end
 local options = {
-      signs = {
-         add = { hl = "DiffAdd", text = "│", numhl = "GitSignsAddNr" },
-         change = { hl = "DiffChange", text = "│", numhl = "GitSignsChangeNr" },
-         delete = { hl = "DiffDelete", text = "", numhl = "GitSignsDeleteNr" },
-         topdelete = { hl = "DiffDelete", text = "‾", numhl = "GitSignsDeleteNr" },
-         changedelete = { hl = "DiffChangeDelete", text = "~", numhl = "GitSignsChangeNr" },
-      },
+	signs = {
+		add = { hl = "DiffAdd", text = "│", numhl = "GitSignsAddNr" },
+		change = { hl = "DiffChange", text = "│", numhl = "GitSignsChangeNr" },
+		delete = { hl = "DiffDelete", text = "", numhl = "GitSignsDeleteNr" },
+		topdelete = { hl = "DiffDelete", text = "‾", numhl = "GitSignsDeleteNr" },
+		changedelete = { hl = "DiffChangeDelete", text = "~", numhl = "GitSignsChangeNr" },
+	},
 }
 gitsigns.setup(options)
-
