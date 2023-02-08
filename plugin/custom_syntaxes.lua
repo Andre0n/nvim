0f48db853943f647bd94316a6381882f55e33a5d
Andre0n <andreon098@gmail.com>
2023-02-08 13:55:56 -0300

    Remove `plugin` directory

--- plugin/custom_syntaxes.lua
+++
@@ -1,9 +0,0 @@
-local syntaxes = {
-    tiny = function()
-        vim.cmd ("au BufNewFile,BufRead *.tny set filetype=tiny ")
-        vim.cmd ("au Syntax tiny runtime! syntax/tiny.vim ")
-    end
-}
-for _, syntax in pairs(syntaxes) do
-    syntax()
-end
